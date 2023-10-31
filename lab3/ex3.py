7eabc22ab3ac7dd57e161788edb8d3187e80bde8
cosminalex <100424444+cosminalex2002@users.noreply.github.com>
2023-10-31 17:40:22 +0200

    added ex3

--- lab3/ex3.py
+++ lab3/ex3.py
@@ -1,33 +1,29 @@
 def compare(dict1, dict2):
-    # ambele s dictionare
     if not isinstance(dict1, dict) or not isinstance(dict2, dict):
         return False
 
-    # lungime la fel
-    if len(dict1) != len(dict2):
+    if set(dict1.keys()) != set(dict2.keys()):
         return False
 
-    for key, value in dict1.items():
-        # daca sunt aceleasi chei in ambele dict
-        if key not in dict2:
-            return False
-
+    for key in dict1:
+        value1 = dict1[key]
         value2 = dict2[key]
 
-        # verif daca mai contin alte dictionare, daca da, apelam recursiv
-        if isinstance(value, dict) and isinstance(value2, dict):
-
-            if not compare(value, value2):
+        if isinstance(value1, dict) and isinstance(value2, dict):
+            if not compare(value1, value2):
                 return False
+        elif isinstance(value1, (list, set, tuple)) and isinstance(value2, (list, set, tuple)):
+            if len(value1) != len(value2):
+                return False
+            for v1, v2 in zip(value1, value2):
+                if v1 != v2:
+                    return False
         else:
-            # daca nu s dict, le comparam direct
-            if value != value2:
+            if value1 != value2:
                 return False
     return True
 
-
 dict1 = {'a': 1, 'b': [1, 2, 5], 'c': {'x': 5, 'y': 6}}
 dict2 = {'a': 1, 'b': [1, 2, 3], 'c': {'x': 5, 'y': 6}}
-
 result = compare(dict1, dict2)
 print(result)
