ec033c30fa5b8c3b2edbf62901472010f74891d8
cosminalex <100424444+cosminalex2002@users.noreply.github.com>
2023-10-31 18:43:45 +0200

    added ex3

--- lab3/ex3.py
+++ lab3/ex3.py
@@ -12,18 +12,17 @@ def compare(dict1, dict2):
         if isinstance(value1, dict) and isinstance(value2, dict):
             if not compare(value1, value2):
                 return False
+
         elif isinstance(value1, (list, set, tuple)) and isinstance(value2, (list, set, tuple)):
-            if len(value1) != len(value2):
+            if len(value1) != len(value2) or any(v1 != v2 for v1, v2 in zip(value1, value2)):
                 return False
-            for v1, v2 in zip(value1, value2):
-                if v1 != v2:
-                    return False
-        else:
-            if value1 != value2:
-                return False
+
+        elif value1 != value2:
+            return False
+
     return True
 
 dict1 = {'a': 1, 'b': [1, 2, 5], 'c': {'x': 5, 'y': 6}}
-dict2 = {'a': 1, 'b': [1, 2, 3], 'c': {'x': 5, 'y': 6}}
+dict2 = {'a': 1, 'b': [1, 2, 5], 'c': {'x': 5, 'y': 6}}
 result = compare(dict1, dict2)
 print(result)
